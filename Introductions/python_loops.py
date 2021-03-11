21eb03e4c83bcda286bab464cb4f1962f131893e
Nitesh <niteshkrsingh51@gmail.com>
2021-03-12 02:16:59 +0530

    Create python_loops.py

---
+++ Introductions/python_loops.py
@@ -0,0 +1,8 @@
+#The provided code stub reads and integer, , from STDIN. For all non-negative integers i<n , print i^2.
+
+if __name__ == '__main__':
+    n = int(input())
+i=0  
+while i < n:
+    print(i*i)
+    i += 1
