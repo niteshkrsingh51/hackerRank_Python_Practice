4c88561710cfda7f4753f2860a24a80f921e3e14
Nitesh <niteshkrsingh51@gmail.com>
2021-03-12 02:17:36 +0530

    Create string_split_join.py

---
+++ strings/string_split_join.py
@@ -0,0 +1,12 @@
+#You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+def split_and_join(line):
+    line_list = line.split(' ')
+    line = '-'.join(line_list)
+    return line
+    
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
