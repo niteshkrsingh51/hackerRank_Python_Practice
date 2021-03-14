55cd2fa74362976e829a9a339bec44343d1a93df
Nitesh <niteshkrsingh51@gmail.com>
2021-03-15 00:53:52 +0530

    Create set_difference.py

---
+++ sets/set_difference.py
@@ -0,0 +1,9 @@
+first_set_length = int(input())
+first_set = set(map(int, input().split()))
+second_set_length = int(input())
+second_set = set(map(int, input().split()))
+sum = 0
+third_set = first_set.difference(second_set)
+final_list = list(third_set)
+final_list_count = len(final_list)
+print(final_list_count)
