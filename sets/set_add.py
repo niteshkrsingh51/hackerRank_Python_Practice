20abfb8a951e3422aa7c3904b7e0759e6d73f0f4
Nitesh <niteshkrsingh51@gmail.com>
2021-03-14 23:57:02 +0530

    Create set_add.py

---
+++ sets/set_add.py
@@ -0,0 +1,14 @@
+set_length = int(input())
+required_list = []
+
+for _ in range(1,set_length+1):
+    required_list.append(input())
+
+required_set = set(required_list)
+print(len(required_set))
+
+
+
+    
+
+
