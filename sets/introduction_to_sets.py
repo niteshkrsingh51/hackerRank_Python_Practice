017a06903ca522d2e8fc93740221d368161d5851
Nitesh <niteshkrsingh51@gmail.com>
2021-03-15 00:24:51 +0530

    Update introduction_to_sets.py

--- sets/introduction_to_sets.py
+++ sets/introduction_to_sets.py
@@ -1,12 +1,9 @@
-
-
 def average(arr):
     new_list = set(arr)
     total_numbers = len(new_list)
     sum_number = sum(new_list)
     average_number = sum_number / total_numbers
     return average_number
-
 
 n = int(input())
 arr = list(map(int, input().split()))
